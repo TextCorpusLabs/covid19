f2bb9c8fc1cb66cae57148bd1555b2d8a1e6417a
Mark Newman <mark@trinetteandmark.com>
2020-03-19 12:33:01 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# COVID-19 To Text Corpus
+
+[Kaggle](https://www.kaggle.com/) has provided an excelent [data source](https://www.kaggle.com/allen-institute-for-ai/CORD-19-research-challenge) for the COVID-19 courtesy of [AI2](https://allenai.org/)
+The purpose of this repo is to convert it from the given format into the normal text corpus format.
+I.E. one document per file, one sentence per line, pargraphs have a blank line between them.
+
+# Prerequisites
+
+The following packages need to be installed.
+I recommend using [Chocolatey](https://chocolatey.org/install).
+
+* [7-zip](https://www.7-zip.org/)
+* [Python](https://www.python.org/downloads/)
+
+  
+```{ps1}
+if('Unrestricted' -ne (Get-ExecutionPolicy)) { Set-ExecutionPolicy Bypass -Scope Process -Force }
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+refreshenv
+
+choco install 7zip.install -y
+choco install python3 -y
+```
+
+# Modules
+
+All scripts have been tested on Python 3.8.2.
+The below modules are need to run the scripts.
+The scripts were tested on the noted versions, so YMMV.
+**Note**: not all modules are required for all scripts.
+If this it the first time running the scripts, the modules will need to be installed.
+They can be installed by navigating to the `~/code` folder, then using the below code.
+
+* nltk 3.4.5
+* progressbar2 3.47.0
+
+```{shell}
+pip install -r requirments.txt
+python -c "import nltk;nltk.download('punkt')"
+```
+
+# Steps
+
+The below document describes how to recreate the text corpus.
+It assumes that a particular path structure will be used, but the commands can be modified to target a different directory structure without changing the code.
+I am choosing the `d:/covid19` directory because my d drive is big enough to hold everything.
+
+1. Clone this repo then open a shell to the `~/code` directory.
+2. Retrieve the dataset _by hand_.
+   Click on the [download](https://www.kaggle.com/allen-institute-for-ai/CORD-19-research-challenge/download) link, saving the file to a know location.   
+3. Extract the data in-place with no folder structure.
+   The `e` switch flattens the extract so the custom code does not need to recursivaly search the folder structure.
+```{shell}
+"C:/Program Files/7-Zip/7z.exe" e -od:/covid19/raw "d:/covid19/*.zip"
+```
+4. [Extract](./code/extract_metadata.py) the meta-data.
+   This will create a single `metadata.csv` containing some useful information.
+   In general this would be used as part of segementation or as part of a MANOVA.
+```{shell}
+python extract_metadata.py -in d:/covid19/raw -out d:/covid19/metadata.csv
+```
+5. [Convert](./code/convert_to_corpus.py) the raw JSON files into the nomal folder corpus format.
+   This will create a text corpus folder at the location called `./corpus` containing 2 sub folders, one for the abstract and one for the body.
+```{shell}
+python convert_to_corpus.py -in d:/covid19/raw -out d:/covid19/corpus
+```
---
+++ code/convert_to_corpus.py
@@ -0,0 +1,87 @@
+from argparse import ArgumentParser
+from collections import namedtuple
+from shutil import rmtree
+import json
+import nltk.tokenize as tok
+import pathlib
+import progressbar as pb
+
+cite_span = namedtuple('cite_span', 'start end')
+
+def convert_to_corpus(folder_in, folder_out):
+    folder_in = pathlib.Path(folder_in)
+    folder_out = pathlib.Path(folder_out)
+    abstract_out = folder_out.joinpath('./abstract')
+    body_out = folder_out.joinpath('./body')
+
+    cleanup_structure(abstract_out)
+    cleanup_structure(body_out)
+
+    i = 1
+    widgets = [ 'Converting File # ', pb.Counter(), ' ', pb.Timer(), ' ', pb.BouncingBar(marker = '.', left = '[', right = ']')]
+    with pb.ProgressBar(widgets = widgets) as bar:
+        for file_name in folder_in.iterdir():
+            if file_name.is_file() and file_name.suffix.lower() == '.json':
+                bar.update(i)
+                i = i + 1
+
+                file_in = file_name
+                with file_in.open('r', encoding = 'utf-8') as file_in:
+                    raw_data = json.load(file_in)
+                
+                abstract = extract_paragraphs(raw_data['abstract'])
+                abstract = convert_to_sentences(abstract)
+                body = extract_paragraphs(raw_data['body_text'])
+                body = convert_to_sentences(body)
+
+                if len(abstract) == 0 or len(body) == 0:
+                    print(f'file {file_name.stem} is not a full text article')
+                else:
+                    write_corpus(abstract_out.joinpath(f'./{file_name.stem}.txt'), abstract)
+                    write_corpus(body_out.joinpath(f'./{file_name.stem}.txt'), body)
+
+def cleanup_structure(folder):
+    if folder.exists():
+        if folder.is_dir():
+            rmtree(folder)
+        else:
+            folder.unlink()
+    folder.mkdir(parents = True)
+
+def extract_paragraphs(paragraphs):
+    result = [clean_paragraph(paragraph) for paragraph in paragraphs]
+    return result
+
+def clean_paragraph(paragraph):
+    text = paragraph['text']
+    cite_spans = paragraph['cite_spans']
+    cite_spans = [cite_span(x['start'], x['end']) for x in cite_spans]
+    for x in sorted(cite_spans, key = lambda x: x.end, reverse = True):
+        text = text[0: x.start:] + text[x.end + 1::]
+    return text
+
+def convert_to_sentences(paragraphs):
+    result = [tok.sent_tokenize(paragraph) for paragraph in paragraphs]
+    return result
+
+def write_corpus(file_out, paragraphs):
+    first = True
+    with file_out.open('w', encoding = 'utf-8') as file_out:
+        for paragraph in paragraphs:
+            if first:
+                first = False
+            else:
+                file_out.write('\n')
+            for sentence in paragraph:
+                file_out.write(sentence)
+                file_out.write('\n')                
+
+if __name__ == '__main__':
+    #parser = ArgumentParser()
+    #parser.add_argument('-in', '--folder-in', help = 'Folder containing the raw JSON files', required = True)
+    #parser.add_argument('-out', '--folder-out', help = 'Folder containing the newly created text corpus', required = True)
+    #args = parser.parse_args()
+    #print(f'folder in: {args.folder_in}')
+    #print(f'folder out: {args.measure_out}')
+    #convert_to_corpus(args.folder_in, args.folder_out)
+    convert_to_corpus('d:/covid19/raw', 'd:/covid19/corpus')
